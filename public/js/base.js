95a8eef4d4e04eb3e1aeb8ffcbf7eba82ab1b378
kaili.wkl <kaili.wkl@alibaba-inc.com>
2015-06-21 19:46:09 +0800

    add three bar

--- public/js/base.js
+++ public/js/base.js
@@ -1,3 +1,18 @@
+
+function handleCategory(){
+    // 如果url带#，直接显示相应分类的posts
+    var categoryId = window.location.hash.substring(1);
+    if(categoryId){
+        showCategory(categoryId);
+    }
+
+    // 点击显示相应分类的 posts
+    $(".category-link").on("click", function(e){
+        var categoryId = $(this)[0].hash.substring(1);
+        showCategory(categoryId);
+    });
+}
+
 /**
  * index页，控制post列表显示
  * 默认全部post
@@ -16,34 +31,18 @@ function showCategory(id){
  * @param {[type]} mq [description]
  */
 function WidthChange(mq){
-    if(mq.matches){
+    if(mq.matches){ //手机，sidebar 默认关闭
         $("#close-btn").attr("checked",true);
+        $("#close-btn-right").attr("checked",true);
     }else{
         $("#close-btn").attr("checked",false);
     }  
 }
 
-$(document).ready(function() {
-    // 如果url带#，直接显示相应分类的posts
-    var categoryId = window.location.hash.substring(1);
-    if(categoryId){
-        showCategory(categoryId);
-    }
-
-    // 如果是手机，sidebar 默认关闭
-    if(window.matchMedia){
-        var mq = window.matchMedia("(max-width: 600px)"); 
-        mq.addListener(WidthChange);   
-        WidthChange(mq); 
-    }   
-
-    // 点击显示相应分类的 posts
-    $(".category-link").on("click", function(e){
-        var categoryId = $(this)[0].hash.substring(1);
-        showCategory(categoryId);
-        // return false;
-    });
-
+/**
+ * 显示评论
+ */
+function showComments(){
     // show comments
     $('body').on('click', '.show-commend', function() {
         var disqus_shortname = $('.show-commend').attr('name');
@@ -54,4 +53,75 @@ $(document).ready(function() {
           cache: true
         });
     });
+}
+
+/**
+ * 给文章加章节目录
+ * @return {[type]} [description]
+ */
+function addPostCatalog(){
+    var $ui = {
+        catalogArea: $('.catalog-list'),
+        postTitle: $('.post-title'),
+        postArticle: $('.post-article')
+    };
+
+    $('<a href="#post-title" title="' + $ui.postTitle.text() + '">' + $ui.postTitle.text() + '</a>').insertBefore('.catalog-list'); // 目录前插入post标题
+
+    // 生成目录
+    $ui.postArticle.children('h1,h2,h3').each(function() {
+        var classTitle = "";
+
+        switch($(this).prop("tagName")){
+            case 'H1':
+                classTitle = "h1";
+                break; 
+            case 'H2':
+                classTitle = "h2";
+                break; 
+            case 'H3':
+                classTitle = "h3";
+                break; 
+            case 'H4':
+                classTitle = "h4";
+                break; 
+            case 'H5':
+                classTitle = "h5";
+                break; 
+            default:;
+        }
+
+        $ui.catalogArea.append('<li>'
+          + '<a href="#' + $(this).attr('id') + '" class="' + classTitle + '" title="' + $(this).text() + '">' + $(this).text() + '</a>'
+        + '</li>');
+    });
+}
+
+/**
+ * 滚动到顶部、底部
+ */
+function scrollTopAndBot() {
+    $('.scroll .top').click(function() {
+        $('html, body').animate({scrollTop:0}, 'fast');
+        return false;
+    });
+
+    $('.scroll .bot').click(function() {
+        $('html, body').animate({scrollTop:$(document).height()}, 'slow');
+        return false;
+    });
+}
+
+$(document).ready(function() {
+    // 如果是手机
+    if(window.matchMedia){
+        var mq = window.matchMedia("(max-width: 600px)"); 
+        mq.addListener(WidthChange);   
+        WidthChange(mq); 
+    }   
+    
+    handleCategory();
+    showComments();
+    addPostCatalog();
+    scrollTopAndBot();
 });
